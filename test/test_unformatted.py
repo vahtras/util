9a1d98159bf444e4d48c3214e98db8b21f978d08
Olav Vahtras <vahtras@kth.se>
2014-08-19 07:13:07 +0200

    Correct path for test files in unformatted

--- test/test_unformatted.py
+++ test/test_unformatted.py
@@ -35,44 +35,49 @@ from ..unformatted import FortranBinary
 class TestUnformatted(unittest.TestCase):
 
     def setUp(self):
+        import os
+        test_dir, _ = os.path.split(__file__)
+        self.fort1 = os.path.join(test_dir, 'fort.1')
+        self.fort2 = os.path.join(test_dir, 'fort.2')
+        self.fort3 = os.path.join(test_dir, 'fort.3')
         pass
 
     def test_1_lengths(self):
-        record_lengths = [rec.reclen for rec in FortranBinary('fort.1')]
+        record_lengths = [rec.reclen for rec in FortranBinary(self.fort1)]
         self.assertListEqual(record_lengths, [4, 24])
 
     def test_1_int(self):
-        fort1 = FortranBinary('fort.1')
+        fort1 = FortranBinary(self.fort1)
         first = fort1.next().read(1, 'i')
         self.assertTupleEqual(first, (3,))
 
     def test_1_floats(self):
-        fort1 = FortranBinary('fort.1')
+        fort1 = FortranBinary(self.fort1)
         fort1.next()
         second = fort1.next().read(3, 'd')
         self.assertTupleEqual(second, (1., 2., 3.))
 
     def test_2_lengths(self):
-        record_lengths = [rec.reclen for rec in FortranBinary('fort.2')]
+        record_lengths = [rec.reclen for rec in FortranBinary(self.fort2)]
         self.assertListEqual(record_lengths, [4, 5])
 
     def test_2_int(self):
-        fort2 = FortranBinary('fort.2')
+        fort2 = FortranBinary(self.fort2)
         first = fort2.next().read(1, 'i')
         self.assertTupleEqual(first, (0,))
 
     def test_2_char(self):
-        fort2 = FortranBinary('fort.2')
+        fort2 = FortranBinary(self.fort2)
         fort2.next()
         second = "".join(fort2.next().read(5, 'c'))
         self.assertEqual(second, 'LABEL')
 
     def test_3_lengths(self):
-        record_lengths = [rec.reclen for rec in FortranBinary('fort.3')]
+        record_lengths = [rec.reclen for rec in FortranBinary(self.fort3)]
         self.assertListEqual(record_lengths, [24, 24])
 
     def test_3_floats(self):
-        fort3 = FortranBinary('fort.3')
+        fort3 = FortranBinary(self.fort3)
         first = fort3.next().read(3, 'd')
         second = fort3.next().read(3, 'd')
         self.assertTupleEqual(second, (5., 6., 7.))
@@ -83,7 +88,7 @@ class TestUnformatted(unittest.TestCase):
             fort.next() #x
             fort.next() #y
             fort.next() #eof
-        self.assertRaises(StopIteration, read_past_eof, 'fort.3')
+        self.assertRaises(StopIteration, read_past_eof, self.fort3)
 
 if __name__ == "__main__":
     unittest.main()
