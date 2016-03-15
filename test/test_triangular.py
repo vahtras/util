f5ec5d0631b7537b2897c78a4486ae0229946523
Olav Vahtras <vahtras@kth.se>
2016-03-15 07:06:29 +0100

    random triangular test

--- test/test_triangular.py
+++ test/test_triangular.py
@@ -6,6 +6,7 @@ class TestTriangular(unittest.TestCase):
 
     def setUp(self):
         self.twodim = triangular.init([1.,  2.,  3.])
+        np.random.seed(0)
 
     def test_init_square_shape(self):
         self.assertEqual(self.twodim.sshape, (2, 2))
@@ -40,6 +41,15 @@ class TestTriangular(unittest.TestCase):
 
     def test_mul(self):
         np.testing.assert_equal(self.twodim*self.twodim, [[5., 8.], [8., 13.]])
+
+    def test_random(self):
+        self.twodim.random()
+        np.testing.assert_allclose(self.twodim, [ 0.5488135 ,  0.71518937,  0.60276338])
+
+    def test_random_antisymmetric(self):
+        As = triangular((2, 2), anti=True)
+        As.random()
+        np.testing.assert_allclose(As, [ 0.0000000 ,  0.71518937,  0.00000000])
 
 if __name__ == "__main__": #pragma: no cover
     unittest.main()
