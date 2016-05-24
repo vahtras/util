7f7bea450566ad22cf360f2e5cfe5cda4ea68aa9
Olav Vahtras <vahtras@kth.se>
2016-05-24 21:10:50 +0200

    Reformatting in testfile

--- tests/test_full.py
+++ tests/test_full.py
@@ -29,8 +29,8 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(this, ref)
 
     def test_str2a(self):
-        M = matrix((2,2))
-        M[0,0] = M[1,1] = 1
+        M = matrix((2, 2))
+        M[0, 0] = M[1, 1] = 1
         this = str(M)
         ref = """
  (2, 2) 
@@ -41,7 +41,7 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(this, ref)
 
     def test_str2b(self):
-        M = matrix((6,6))
+        M = matrix((6, 6))
         for i in range(6):
             M[i, i] = float(i)
         this = str(M)
@@ -59,8 +59,8 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(this, ref)
 
     def test_str3(self):
-        M = matrix((2,2,2))
-        M[0,0,0] = M[1,1,1] = 1
+        M = matrix((2, 2, 2))
+        M[0, 0, 0] = M[1, 1, 1] = 1
         this = str(M)
         ref = """
  (2, 2, 2) 
@@ -76,8 +76,8 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(this, ref)
 
     def test_str4(self):
-        M = matrix((2,2,2,2))
-        M[0,0,0,0] = M[1,1,1,1] = 1
+        M = matrix((2, 2, 2, 2))
+        M[0, 0, 0, 0] = M[1, 1, 1, 1] = 1
         this = str(M)
         ref = """
  (2, 2, 2, 2) 
@@ -117,8 +117,8 @@ class TestMatrix(unittest.TestCase):
         np.testing.assert_equal(M*2, [[4, 2], [2, 4]])
 
     def test_outer(self):
-        V1 = matrix((2, )); V1[0]=1.0
-        V2 = matrix((2, )); V2[1]=1.0
+        V1 = matrix((2,)); V1[0] = 1.0
+        V2 = matrix((2,)); V2[1] = 1.0
         V12 = V1.x(V2)
         np.testing.assert_equal(V12, [[0, 1], [0, 0]])
 
@@ -167,14 +167,14 @@ class TestMatrix(unittest.TestCase):
         M = matrix((2, 2))
         M1 = matrix((3, 3)); M1[1, 1] = 5
         M[0, 0] = 1; M[0, 1] = 2; M[1, 0] = 3; M[1, 1] = 4
-        M.scatteradd(M1, [0, 2], [0, 2]) 
+        M.scatteradd(M1, [0, 2], [0, 2])
         np.testing.assert_equal(M1, [[1, 0, 2], [0, 5, 0], [3, 0, 4]])
 
     def test_pack(self):
         M = matrix((2, 2))
         M1 = matrix((3, 3))
         M1[0, 0] = 1; M1[0, 2] = 2; M1[2, 0] = 3; M1[2, 2] = 4
-        M1.packto(M, [0, 2], [0, 2]) 
+        M1.packto(M, [0, 2], [0, 2])
         np.testing.assert_equal(M, [[1, 2], [3, 4]])
 
     def test_pack_rows(self):
@@ -222,6 +222,10 @@ class TestMatrix(unittest.TestCase):
         M = init([[4, 1], [1, 3]])
         np.testing.assert_almost_equal(M.cofactor(), [[3., -1.], [-1., 4.]])
 
+    def test_cofactor_inverse(self):
+        M = init([[4, 1], [1, 3]])
+        np.testing.assert_allclose(M.inv(), M.cofactor().T/M.det())
+
     def test_cofactor_1by1(self):
         M = init([[3.]])
         np.testing.assert_almost_equal(M.cofactor(), [[1.0]])
@@ -232,13 +236,21 @@ class TestMatrix(unittest.TestCase):
 
     def test_eigvec(self):
         M = init([[0, 1], [1, 0]])
-        np.testing.assert_almost_equal(sqrt(2)*M.eigvec()[1], [[-1., 1.],[1,1]])
+        np.testing.assert_almost_equal(
+            sqrt(2)*M.eigvec()[1],
+            [[-1., 1.], [1., 1.]]
+            )
 
     def test_qr_Q(self):
         A = init([[12, 6, -4], [-51, 167, 24], [4, -68, -41]])
         Q, R = A.qr()
-        np.testing.assert_almost_equal(Q, 
-            -init([[6./7, 3./7, -2./7], [-69./175, 158./175, 6./35], [-58./175, 6./175, -33./35]])
+        np.testing.assert_almost_equal(
+            Q,
+            -init(
+                [[6./7, 3./7, -2./7],
+                [-69./175, 158./175, 6./35],
+                [-58./175, 6./175, -33./35]]
+                )
             )
     def test_qr_R(self):
         A = init([[12, 6, -4], [-51, 167, 24], [4, -68, -41]])
