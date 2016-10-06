9c9319516875875707f1d922d9dd20d9f2c1ce6e
Olav Vahtras <vahtras@kth.se>
2016-10-06 09:58:49 +0200

    Change name taken in pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from util import __version__
 
 setup(
-    name="util",
+    name="blocked-matrix-utils",
     author="Olav Vahtras",
     author_email="olav.vahtras@gmail.com",
     version=__version__,
