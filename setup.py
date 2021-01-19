6edd3258090c3331041a15d11806ad5a67834972
Olav Vahtras <vahtras@kth.se>
2021-01-19 17:19:37 +0100

    transfer to pep517 install

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
 from setuptools import setup
-from util import __version__
 
-setup(
-    name="blocked-matrix-utils",
-    author="Olav Vahtras",
-    author_email="olav.vahtras@gmail.com",
-    version=__version__,
-    license="LICENSE.txt",
-    description="NumPy wrappers for blocked matrices",
-    url="https://github.com/vahtras/util",
-    packages=["util"],
-    install_requires=["scipy"],
-)
+setup()
